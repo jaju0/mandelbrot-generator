d471ff8a6172e1ba8749cdc56dd49d3d2891fd1b
jaju0 <jannes_oltmanns@hotmail.de>
2024-01-09 23:27:12 +0100

    added temporary test to main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include "Generator.hpp"
+#include "Image.hpp"
+#include "EscapeTimeAlgos/EscapeTimeAlgoDistanceEstimation.hpp"
+#include "ColoringStrategies/ColoringStrategyBasicDistanceEstimation.hpp"
+
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    auto escapeTimeAlgo = std::make_shared<mandelbrot::EscapeTimeAlgoDistanceEstimation>();
+    escapeTimeAlgo->setMaxIterations(10000);
+    auto coloringStrategy = std::make_shared<mandelbrot::ColoringStrategyBasicDistanceEstimation>();
+    mandelbrot::Generator generator(std::thread::hardware_concurrency(), 1920, 1080, 10000, escapeTimeAlgo);
+    generator.next();
+    auto result = generator.getResult();
+    mandelbrot::Image image(result, 1920, 1080, coloringStrategy);
+    image.saveAsJPEG("test.jpg");
+    generator.exit();
     return 0;
 }
