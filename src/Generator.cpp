6eb0b9b7e89f13c86bc1ee322e388a24af61cf24
jaju0 <jannes_oltmanns@hotmail.de>
2024-01-12 11:54:19 +0100

    implemented GeneratingStrategy dependency in Generator

--- src/Generator.cpp
+++ src/Generator.cpp
@@ -4,82 +4,28 @@
 
 namespace mandelbrot {
 
-Generator::Generator(uint32_t numberOfThreads, uint32_t imageWidth, uint32_t imageHeight, uint32_t maxIterations, std::shared_ptr<EscapeTimeAlgo> pEscapeTimeAlgo)
+Generator::Generator(uint32_t numberOfThreads, std::shared_ptr<GeneratingStrategy> pGeneratingStrategy, std::shared_ptr<EscapeTimeAlgo> pEscapeTimeAlgo)
     : m_numberOfThreads(numberOfThreads)
     , m_availableThreads(m_numberOfThreads)
-    , m_imageWidth(imageWidth)
-    , m_imageHeight(imageHeight)
-    , m_maxIterations(maxIterations)
     , m_shouldTerminate(false)
-    , m_calculator(m_ioc, m_numberOfThreads, pEscapeTimeAlgo)
+    , m_pGeneratingStrategy(pGeneratingStrategy)
 {
+    m_pCalculator = std::make_shared<Calculator>(m_ioc, m_numberOfThreads, pEscapeTimeAlgo);
+    m_pGeneratingStrategy->setCalculator(m_pCalculator);
+    m_pGeneratingStrategy->setExecutionFunction(std::bind(&Generator::execute, this));
+
     for(uint32_t i = 0; i < m_numberOfThreads; ++i)
         m_threads.emplace_back(&Generator::threadLoop, this);
 }
 
 void Generator::next()
 {
-    // TODO
-    std::error_code ec = createImage(0.0L, 0.0L, 1.0L, m_maxIterations);
-    if(ec == error::precision_issue)
-    {
-        // TODO: reset parameters
-    }
-    if(ec)
-        throw ec;
+    m_pGeneratingStrategy->generate();
 }
 
 Calculator::Result& Generator::getResult()
 {
-    return m_calculator.getResult();
-}
-
-std::error_code Generator::createImage(long double centerX, long double centerY, long double zoomFactor, uint32_t iterations)
-{
-    long double aspectRatio = static_cast<long double>(m_imageWidth) / static_cast<long double>(m_imageHeight);
-
-    long double halfY = 0.5 * 2.24 * zoomFactor;
-    long double halfX = aspectRatio * halfY;
-
-    long double minImag = centerY - halfY;
-    long double maxImag = centerY + halfY;
-    long double minReal = centerX - halfX;
-    long double maxReal = centerX + halfX;
-
-    if(!isPrecisionValid(minImag, maxImag, m_imageHeight) || !isPrecisionValid(minReal, maxReal, m_imageWidth))
-        return mandelbrot::error::precision_issue;
-
-    m_calculator.setParameters(Calculator::Parameters {
-        m_imageWidth,
-        m_imageHeight,
-        minReal,
-        maxReal,
-        minImag,
-        maxImag,
-        iterations,
-    });
-
-    m_calculator.postTasks();
-    m_cvIocExecution.notify_all();
-    waitForResult();
-
-    return std::error_code();
-}
-
-bool Generator::isPrecisionValid(long double min, long double max, uint32_t size)
-{
-    long double diff = max-min;
-    long double last = 0.0L;
-    for(uint32_t i = 0; i < size; ++i)
-    {
-        long double current = min + static_cast<long double>(i) * diff / static_cast<long double>(size);
-        if(current == last)
-            return false;
-
-        last = current;
-    }
-
-    return true;
+    return m_pCalculator->getResult();
 }
 
 void Generator::exit()
@@ -97,6 +43,14 @@ void Generator::waitForResult()
     std::unique_lock lk(m_cvWorkDoneMutex);
     m_cvWorkDone.wait(lk);
 }
+
+void Generator::execute()
+{
+    m_pCalculator->postTasks();
+    m_cvIocExecution.notify_all();
+    waitForResult();
+}
+
 
 void Generator::threadLoop()
 {
